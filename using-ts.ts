c68290dc84d63da32274084e9600e731c337f3cf
Abdullahi Adam <AbdullahiAdam.aa@gmail.com>
2021-10-01 01:13:49 +0100

    Dont mind this just testing

--- using-ts.ts
+++ using-ts.ts
@@ -1,10 +1,10 @@
 const button = document.querySelector("button");
-const input1 = document.getElementById("num1");
-const input2 = document.getElementById("num2");
-function add(num1, num2) {
+const input1 = document.getElementById("num1")! as HTMLInputElement;
+const input2 = document.getElementById("num2")! as HTMLInputElement;
+function add(num1: number, num2: number) {
   return num1 + num2;
 }
 
 button.addEventListener("click", function () {
-  console.log(add(input1.value, input2.value));
+  console.log(add(+input1.value, +input2.value));
 });
