d1a24771b5acb80a6c24a1aac4890a1b57e20e1e
Abdullahi Adam <AbdullahiAdam.aa@gmail.com>
2021-10-01 23:16:07 +0100

    live server test

---
+++ app.js
@@ -0,0 +1,12 @@
+var person = {
+    name: "John",
+    age: 34
+};
+function add(num1, num2, check) {
+    return check && num1 + num2;
+}
+var number1 = 54;
+var number2 = 32;
+var isTrue = true;
+person.name = "Ado";
+console.log(person);
---
+++ app.ts
@@ -0,0 +1,18 @@
+let person: {
+  name: string;
+  age: number;
+} = {
+  name: "John",
+  age: 34,
+};
+
+function add(num1: number, num2: number, check: boolean) {
+  return check && num1 + num2;
+}
+
+const number1 = 54;
+const number2 = 32;
+const isTrue = true;
+
+person.name = "Ado";
+console.log(person);
