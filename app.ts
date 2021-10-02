653aff001e490b1835637bd866609f049e9fb6cc
Abdullahi Adam <AbdullahiAdam.aa@gmail.com>
2021-10-02 09:29:53 +0100

    live server test

--- app.ts
+++ app.ts
@@ -1,14 +1,11 @@
-const person: {
-  name: string;
-  age: number;
-  hobbies: string[];
-  role: [number, string];
-} = {
-  name: "Maximilian",
-  age: 30,
-  hobbies: ["Sports", "Cooking"],
-  role: [2, "author"],
-};
-
-let favoriteActivities: string[];
-favoriteActivities = ["Sports"];
+function add(n1: number, n2: number) {
+  return n1 + n2;
+  }
+  function printResult (num: number): void {
+  console.log( "Result:", num);
+  }
+  // function addAndHandle(n1: number, n2: number)
+  printResult(add(5, 12)) ;
+  let combineValues: (a: number, b: number)
+  number;
+  combineValues = add;
